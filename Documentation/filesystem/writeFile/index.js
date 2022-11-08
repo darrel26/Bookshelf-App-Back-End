a2fe70f93d76647a5504633bb1ee394bf226cbd6
Noid <dionisiusgunadi@outlook.com>
2022-11-08 17:25:27 +0700

    delete documentation folder

--- Documentation/filesystem/writeFile/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const writeableStream = fs.createWriteStream('output.txt');
-
-writeableStream.write('First row!\n');
-writeableStream.write('Second row!\n');
-writeableStream.end('End row!\n');
