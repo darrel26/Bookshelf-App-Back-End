eefa65e20ef6257794e6b1ef8a5d132c3569741c
Noid <dionisiusgunadi@outlook.com>
2022-11-08 13:07:23 +0700

    add route for add book

---
+++ src/handler.js
@@ -0,0 +1,67 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const id = nanoid(16);
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  if (!name) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    }).code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    }).code(400);
+  }
+
+  const readStatus = () => {
+    if (pageCount === readPage) {
+      return true;
+    }
+    return false;
+  };
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished: readStatus(),
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+  if (isSuccess) {
+    return h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    }).code(201);
+  }
+
+  return h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  }).code(501);
+};
+
+module.exports = { addBookHandler };
